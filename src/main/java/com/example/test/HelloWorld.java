61e57d6a3fe65c0ac30a988e370a47a09df2b160
lanling.zjw <lanling.zjw@alibaba-inc.com>
2020-12-09 17:46:53 +0800

    commit init

---
+++ src/main/java/com/example/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+/**
+ * Description: date: 2020/12/09 17:45
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world!");
+    }
+
+
+}
---
+++ src/test/java/com/example/test/HelloworldTest.java
@@ -0,0 +1,10 @@
+package com.example.test;
+
+/**
+ * Description: date: 2020/12/09 17:46
+ */
+public class HelloworldTest {
+
+
+
+}
